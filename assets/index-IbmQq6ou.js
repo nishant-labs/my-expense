0898c7ef89c16db67e4a7397c602e4137bf38f70
nishant-labs <nishant-labs@users.noreply.github.com>
2023-12-23 23:48:58 +0000

    deploy: 99cc3331c6eb4f4515afce4c6267a5c3e3490c95

--- assets/index-IbmQq6ou.js
+++
@@ -1,7 +0,0 @@
-import { j as jsxRuntimeExports } from "./vendor-5_Ts9y2q.js";
-const Summary = () => {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { children: "Work in progress" });
-};
-export {
-  Summary as default
-};
