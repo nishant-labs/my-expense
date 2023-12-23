49162f038b078be2a2547190839994550dd62425
Nishant Kumar Sinha <4038289+nishant-labs@users.noreply.github.com>
2023-12-23 00:24:48 +0000

    task: api host external mapping

--- client/src/api/ApiBase.ts
+++ client/src/api/ApiBase.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { ApiResponse, ApiError } from './types';
 
-const basePath = 'http://localhost:7800';
+declare global {
+	interface Window {
+		EXPENSE_API_HOST: string;
+	}
+}
+
+const basePath = () => window.EXPENSE_API_HOST ?? 'http://localhost:7800';
 
 interface ApiSuccess<T> {
 	data: T;
@@ -9,7 +15,7 @@ interface ApiSuccess<T> {
 
 export async function handlePostCall<T>(path: string, payload: unknown): ApiResponse<T> {
 	try {
-		const response = await axios.post<ApiSuccess<T>>(`${basePath}${path}`, payload);
+		const response = await axios.post<ApiSuccess<T>>(`${basePath()}${path}`, payload);
 		return response.data?.data;
 	} catch (error) {
 		return { error } as ApiError;
@@ -18,7 +24,7 @@ export async function handlePostCall<T>(path: string, payload: unknown): ApiResp
 
 export async function handleGetCall<T>(path: string): ApiResponse<T> {
 	try {
-		const response = await axios.get<ApiSuccess<T>>(`${basePath}${path}`);
+		const response = await axios.get<ApiSuccess<T>>(`${basePath()}${path}`);
 		return response.data?.data;
 	} catch (error) {
 		return { error } as ApiError;
@@ -27,7 +33,7 @@ export async function handleGetCall<T>(path: string): ApiResponse<T> {
 
 export async function handleDeleteCall<T>(path: string): ApiResponse<T> {
 	try {
-		const response = await axios.delete<ApiSuccess<T>>(`${basePath}${path}`);
+		const response = await axios.delete<ApiSuccess<T>>(`${basePath()}${path}`);
 		return response.data?.data;
 	} catch (error) {
 		return { error } as ApiError;
@@ -36,7 +42,7 @@ export async function handleDeleteCall<T>(path: string): ApiResponse<T> {
 
 export async function handlePutCall<T>(path: string, payload: unknown): ApiResponse<T> {
 	try {
-		const response = await axios.put<ApiSuccess<T>>(`${basePath}${path}`, payload);
+		const response = await axios.put<ApiSuccess<T>>(`${basePath()}${path}`, payload);
 		return response.data?.data;
 	} catch (error) {
 		return { error } as ApiError;
