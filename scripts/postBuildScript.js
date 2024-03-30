fd3056a352e6a13e65bafa670827c4e936d9cdd3
Nishant Kumar Sinha <4038289+nishant-labs@users.noreply.github.com>
2024-03-30 21:39:46 +0000

    feature: streamline build for ec2

---
+++ scripts/postBuildScript.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const SERVER_HOST = process.env.SERVER_HOST;
+const SERVER_PORT = process.env.SERVER_PORT;
+
+console.group('PostProcessing config.json');
+console.log('SERVER_HOST: ', SERVER_HOST);
+console.log('SERVER_PORT: ', SERVER_PORT);
+console.groupEnd();
+
+fs.writeFileSync(
+	'./client/dist/config.json',
+	JSON.stringify({
+		baseUrl: `${SERVER_HOST}:${SERVER_PORT}`,
+	}),
+);
--- server/src/constants/environment.ts
+++ server/src/constants/environment.ts
@@ -1,5 +1,5 @@
 // ENV
-export const PORT: number = parseInt(process.env.PORT || '7800');
+export const PORT: number = parseInt(process.env.SERVER_PORT || '7800');
 
 // Database
 export const MONGO_DB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL;
