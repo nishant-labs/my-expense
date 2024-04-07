809bc19206fd76fafaa14572c1d88d2a995f2d5b
nishant-labs <nishant-labs@users.noreply.github.com>
2024-04-07 20:33:39 +0000

    deploy: bec1449b3ca50d2d7beb13bcb5ac74b4017e0512

---
+++ client/assets/index-CYPqh4D1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-Ce-Gnf-x.js";const o=()=>r.jsx("div",{children:"Work in progress"});export{o as default};
--- client/assets/index-DOhDrlyi.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./vendor-yZUb1w_a.js";const o=()=>r.jsx("div",{children:"Work in progress"});export{o as default};
